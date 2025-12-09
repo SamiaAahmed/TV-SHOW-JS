1160b9c98493fb81e4ae6da8ccbed1490828bdd6
Samia Ahmed <23-101162@students.eui.edu.eg>
2025-12-09 23:02:18 +0200

    section1

--- home.js
+++ home.js
@@ -1,2 +1,7 @@
 document.getElementById("1").innerHTML = "An elite team of FBI behavioral analysts hunts the nation's most dangerous criminals.";
 document.getElementById("2").innerHTML = "Discover More";
+document.getElementById("3").innerHTML = "FBI Agents";
+document.getElementById("4").innerHTML = "Spencer Reid";
+document.getElementById("5").innerHTML = "Darek Morgan";
+document.getElementById("6").innerHTML = "Emily Prentiss";
+document.getElementById("7").innerHTML = "Jennifer Jareau";
