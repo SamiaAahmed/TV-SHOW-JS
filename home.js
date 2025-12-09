ff75dd7c942adb96f74fa1ce2397e6177de1dc6a
Samia Ahmed <23-101162@students.eui.edu.eg>
2025-12-09 14:43:11 +0200

    hero

---
+++ home.js
@@ -0,0 +1,2 @@
+document.getElementById("1").innerHTML = "An elite team of FBI behavioral analysts hunts the nation's most dangerous criminals.";
+document.getElementById("2").innerHTML = "Discover More";
